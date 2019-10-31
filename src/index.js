492bae2b497d485b0b7c6b3a2b04aa263e140cca
Wo1kowsky <43152077+Wo1kowsky@users.noreply.github.com>
2019-10-31 21:33:34 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let value = document.getElementById("value");
+let power = document.getElementById("power");
+
+value.oninput = calculate;
+power.oninput = calculate;
+
+function calculate() {
+    document.getElementById("result").textContent = Math.pow(value.value, power.value)+"";
+}
